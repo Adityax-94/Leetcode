4e9c7708804be6dca9c61ce89d1cec9ec73428ca
Aditya Chavan <166897785+Adityax-94@users.noreply.github.com>
2026-04-03 23:06:37 +0530

    Time: 0 ms (100%), Space: 52.9 MB (93.67%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    
+    for (let i=0;i<nums.length;){
+
+        if (nums[i]==val){
+
+            nums.splice(i,1)
+        }else{
+            i++
+        }
+    }
+    return nums.length
+    
+};
