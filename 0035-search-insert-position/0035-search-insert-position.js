c23e9a199d08b063870be61a2010ba04ad082d28
Aditya Chavan <166897785+Adityax-94@users.noreply.github.com>
2026-04-04 10:10:51 +0530

    Time: 0 ms (100%), Space: 54.3 MB (20.24%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+
+    for(i=0;nums[i]<=target;i++){
+
+        if(nums[i]==target){
+            return i
+        }
+    }
+    return i
+};
