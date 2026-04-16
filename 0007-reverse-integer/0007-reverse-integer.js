964813c1f7ad75e3a07d55d0373461cb5e3b0b2a
Aditya Chavan <166897785+Adityax-94@users.noreply.github.com>
2026-04-16 22:57:56 +0530

    Time: 63 ms (12.32%), Space: 55.6 MB (74.48%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+
+    let sign=0
+    let op=""
+    num=x.toString()
+    // console.log(num)
+    for (let i=num.length-1;i>=0;i--)
+    {
+        if(num[i]=="-")
+        {
+            sign=1
+        }else{
+        op=op+num[i]
+        }
+    }
+    //  console.log(op)
+    
+
+    if(sign==1){
+
+        num1= +op
+        // console.log(num1)
+         if (num1 < -(2**31) || num1 > (2**31 - 1)) {
+        return 0
+    }
+        return num1- (num1*2)
+    
+    }else{
+        num1=+op
+         if (num1 < -(2**31) || num1 > (2**31 - 1)) {
+        return 0
+    }
+            return num1
+    }
+}
+
